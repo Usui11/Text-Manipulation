bbb4d56b8051a5f49926a6bba11632607a850b09
Usui11 <zchoeda101@gmail.com>
2019-12-06 13:36:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-9d8jza
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-9d8jza)
---
+++ index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fontColor : "black", 
+      fontSize:  30
+    };
+  }
+   changeColor = (value) =>{
+     this.setState({
+       fontColor:value
+     })
+   }
+   increaseFont = () =>{
+     this.setState({
+       fontSize:value
+     })
+   }
+  render() {
+    return (
+      <div>
+        <p style={{color: `${this.state.fontColor}`, fontSize: `${this.state.fontSize}px`}}>I am Sonam</p>
+        <button onClick = {() => this.changeColor('red')}>Red</button>
+        <button onClick = {() => this.changeColor('blue')}>Blue</button>
+        <button onClick = {() => this.changeColor('yellow')}>Yellow</button>
+        <button onClick = {() => this.increaseFont}>Size++</button>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
